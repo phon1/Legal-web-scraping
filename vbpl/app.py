a7bd9df7a33c4ba0795cfdefa861d7a276ed3ac2
phon1 <thanhphong2k.bp@gmail.com>
2023-08-25 00:59:06 -0400

    Thay đổi logic code

--- vbpl/app.py
+++ vbpl/app.py
@@ -1,7 +1,5 @@
 import json
-import re
 import time
-from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 import requests
 import threading
@@ -10,13 +8,20 @@ storage = set()
 storeValue = set()
 id_law = 1
 
-MAX_RETRIES = 20  # Số lần retry tối đa
-TIMEOUT_DELAY = 2  # Thời gian delay (giây) sau khi gặp timeout
+MAX_RETRIES = 20
+TIMEOUT_DELAY = 2
+
+storeValueLock = threading.Lock()
+idLawLock = threading.Lock()
+
+json_lock = threading.Lock()
+
 
 def save_to_json(data):
-    with open('law.json', 'w', encoding='utf-8') as json_file:
-        json.dump(data, json_file, ensure_ascii=False, indent=4)
-    print(f'\nThêm data vào json:\nid = {data[-1]["id"]}, \ntitle = {data[-1]["title"]}, \ncontent = {data[-1]["content"]},\nlink = {data[-1]["link"]}')
+    with json_lock:
+        with open('law.json', 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+        print(f'\nThêm data vào json:\nid = {data[-1]["id"]}, \ntitle = {data[-1]["title"]}, \ncontent = {data[-1]["content"]},\nlink = {data[-1]["link"]}')
 
 def update_json_data(data, id, title, content, link):
     for entry in data:
@@ -27,93 +32,14 @@ def update_json_data(data, id, title, content, link):
             return True
     return False
 
-def URL_first_crawl():
+def Law_loop(path):
     global id_law
     global storage
     global storeValue
 
-    path = "/Pages/chitiethoidap.aspx?ItemID=87573"
     storage.add(path)
-    storeValue.add(path)
 
     URL = 'https://vbpl.vn' + path
-
-    retries = 0
-    while retries < MAX_RETRIES:
-        try:
-            response = requests.get(URL, timeout=3)
-            response.raise_for_status()
-            html_content = response.text
-
-            soup = BeautifulSoup(html_content, "html.parser")
-            box_traloi = soup.find_all("div", class_="box_traloi")
-
-            id = id_law
-            link = 'https://vbpl.vn' + path
-
-            title_p = box_traloi[0].find("p", class_="title")
-            title = title_p.find_next("p").get_text(strip=True) if title_p else None
-
-            if title is None or title.strip() == '':
-                retries += 1
-                time.sleep(TIMEOUT_DELAY)
-                print(f'Retrying due to empty title. Retry attempt: {retries}')
-                continue
-
-            try:
-                content_non_p = box_traloi[1].find("p", class_=None)
-                if content_non_p:
-                    send_date_tag = box_traloi[1].find("p", class_="send_date")
-                    send_date_tag.extract()
-
-                    find_title_reply = box_traloi[1].find("p", class_="title_reply")
-                    find_title_reply.extract()
-
-                    content = box_traloi[1].get_text(separator="\n",strip=True)
-            except AttributeError:
-                content = ""
-            
-            entry = {
-                "id": id,
-                "title": title,
-                "content": content,
-                "link": link
-            }
-
-            id_law +=1
-
-            json_data = [entry]
-            save_to_json(json_data)
-
-            paths_list = []
-            links_other_question = soup.find_all("div", class_="news-other box-news")
-            for div in links_other_question:
-                links = div.find_all("a")
-                for link in links:
-                    path = link.get("href")
-                    paths_list.append(path)
-
-            storeValue.update(paths_list)
-
-        except requests.exceptions.RequestException as e:
-            print("Exception:", e)
-            print("Retrying in", TIMEOUT_DELAY, "seconds...")
-            retries += 1
-            time.sleep(TIMEOUT_DELAY)
-
-
-def Law_recursion(path):
-    global id_law
-    global storage
-    global storeValue
-
-    storage.add(path)
-    storeValue.add(path)
-
-    if id_law == 1:
-        URL = 'https://vbpl.vn/Pages/chitiethoidap.aspx?ItemID=87573'
-    else:
-        URL = 'https://vbpl.vn' + path
 
     retries = 0
     while retries < MAX_RETRIES:
@@ -147,10 +73,13 @@ def Law_recursion(path):
             except AttributeError:
                 content = ""
 
-            if not title :
+            if not title:
                 time.sleep(2)
-                print (f'\nAPI die at: ID = {id}, Link = {link}')
-                return Law_recursion(path)
+                print(f'\nAPI die at: ID = {id}, Link = {link}')
+                continue
+
+            with idLawLock:
+                id_law += 1
 
             entry = {
                 "id": id,
@@ -170,28 +99,16 @@ def Law_recursion(path):
 
             save_to_json(json_data)
 
-            paths_list = []
-            links_other_question = soup.find_all("div", class_="news-other box-news")
-            for div in links_other_question:
-                links = div.find_all("a")
-                for link in links:
-                    path = link.get("href")
-                    paths_list.append(path)
+            with storeValueLock:
+                links_other_question = soup.find_all("div", class_="news-other box-news")
+                for div in links_other_question:
+                    links = div.find_all("a")
+                    for link in links:
+                        new_path = link.get("href")
+                        if new_path not in storeValue and new_path not in storage:
+                            storeValue.add(new_path)
 
-
-            for path_dif in paths_list:
-                if path_dif not in storeValue:
-                    storeValue.add(path_dif)
-
-            path_new = next(iter(storeValue  - {path} - storage), None)
-
-            if path_new == None:
-                return
-            if path_new:
-                id_law += 1
-                return Law_recursion(path_new)
-
-            return
+            break
 
         except requests.exceptions.RequestException as e:
             print("Exception:", e)
@@ -199,16 +116,99 @@ def Law_recursion(path):
             retries += 1
             time.sleep(TIMEOUT_DELAY)
 
-if __name__ == "__main__":
-    storage.add("/Pages/chitiethoidap.aspx?ItemID=87573")
-    storeValue.add("/Pages/chitiethoidap.aspx?ItemID=87573")
-    urls_to_crawl = URL_first_crawl()
+def URL_first_crawl(path):
+    global id_law
+    global storage
+    global storeValue
+
+    URL = 'https://vbpl.vn' + path
+
+    retries = 0
+    while retries < MAX_RETRIES:
+        try:
+            response = requests.get(URL, timeout=3)
+            response.raise_for_status()
+            html_content = response.text
+
+            soup = BeautifulSoup(html_content, "html.parser")
+            box_traloi = soup.find_all("div", class_="box_traloi")
+
+            id = id_law
+            link = 'https://vbpl.vn' + path
+
+            title_p = box_traloi[0].find("p", class_="title")
+            title = title_p.find_next("p").get_text(strip=True) if title_p else None
+
+            if title is None or title.strip() == '':
+                retries += 1
+                time.sleep(TIMEOUT_DELAY)
+                print(f'Retrying due to empty title. Retry attempt: {retries}')
+                continue
+
+            with idLawLock:
+                id_law += 1
+                
+            try:
+                content_non_p = box_traloi[1].find("p", class_=None)
+                if content_non_p:
+                    send_date_tag = box_traloi[1].find("p", class_="send_date")
+                    send_date_tag.extract()
+
+                    find_title_reply = box_traloi[1].find("p", class_="title_reply")
+                    find_title_reply.extract()
+
+                    content = box_traloi[1].get_text(separator="\n",strip=True)
+            except AttributeError:
+                content = ""
+
+            entry = {
+                "id": id,
+                "title": title,
+                "content": content,
+                "link": link
+            }
+
+            json_data = [entry]
+            save_to_json(json_data)
+
+            with storeValueLock:
+                paths_list = []
+                links_other_question = soup.find_all("div", class_="news-other box-news")
+                for div in links_other_question:
+                    links = div.find_all("a")
+                    for link in links:
+                        path = link.get("href")
+                        paths_list.append(path)
+                storeValue.update(paths_list)
+
+            break
+
+        except requests.exceptions.RequestException as e:
+            print("Exception:", e)
+            print("Retrying in", TIMEOUT_DELAY, "seconds...")
+            retries += 1
+            time.sleep(TIMEOUT_DELAY)
+
+def main():
+    global storage
+    global storeValue
+    path = '/Pages/chitiethoidap.aspx?ItemID=87573'
+
+    storage.add(path)
+
+    URL_first_crawl(path)
 
     threads = []
-    for url in storeValue:
-        thread = threading.Thread(target=Law_recursion, args=(url,))
-        threads.append(thread)
-        thread.start()
+    while storeValue:
+        path = storeValue.pop()
+        if path not in storage:
+            Law_loop(path)
+            # thread = threading.Thread(target=Law_loop, args=(path,))
+            # thread.start()
+            # threads.append(thread)
 
-    for thread in threads:
-        thread.join()
+    # for thread in threads:
+    #     thread.join()
+
+if __name__ == "__main__":
+    main()
